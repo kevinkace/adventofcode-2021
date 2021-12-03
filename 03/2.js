591d3f106ba44040a0e623832ebb6e40b94ff312
Kevin Cameron <kevinacameron@hotmail.com>
2021-12-02 22:47:51 -0800

    3/2

---
+++ 03/2.js
@@ -0,0 +1,52 @@
+const { readFile } = require("fs/promises");
+
+function bisect(arr, dir, pos, pref) {
+    const l = [];
+    const r = [];
+
+    arr.forEach(line => {
+        (line[pos] === "0" ? l : r).push(line);
+    });
+
+    if (l.length === r.length) {
+        return pref === "0" ? l : r;
+    }
+
+    if (dir) {
+        return l.length > r.length ? l : r;
+    }
+
+    return l.length < r.length ? l : r;
+}
+
+function getRating(arr, dir, pos, pref) {
+    if (arr.length === 1) {
+        return arr[0];
+    }
+
+    const side = bisect(arr, dir, pos, pref);
+
+    // return side;
+
+    return getRating(side, dir, ++pos, pref);
+}
+
+readFile("./input.txt", "utf8")
+// readFile("./sample.txt", "utf8")
+.then(diagRep => {
+    const lines = diagRep.split("\r\n");
+
+    let o2 = getRating(lines, true, 0, "1");
+    let co2 = getRating(lines, false, 0, "0");
+
+    console.log({ o2, co2 });
+
+    // epsilon = gamma.map(dig => (dig < 0 ? 1 : 0)).join("");
+    // gamma = gamma.map(dig => (dig > 0 ? 1 : 0)).join("");
+
+    o2 = parseInt(o2, 2);
+    co2 = parseInt(co2, 2);
+
+
+    console.log({ o2, co2, pr : o2 * co2 });
+});
