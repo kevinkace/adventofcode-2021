ecc089dab94e800fb37f7f7cade060e50d10490f
Kevin Cameron <kevinacameron@hotmail.com>
2021-12-07 23:42:28 -0800

    07/2

--- 07/2.js
+++ 07/2.js
@@ -1,6 +1,6 @@
 const { readFile } = require("fs/promises");
 
-const MAX = 100000;
+const MAX_ITERATIONS = 100000;
 let iterations = 0;
 
 function getTriangle(dist) {
@@ -21,46 +21,34 @@ function getFuel(arr, mid) {
     return dists;
 }
 
-// readFile("./07/input.txt", "utf8")
-readFile("./07/sample.txt", "utf8")
+readFile("./07/input.txt", "utf8")
+// readFile("./07/sample.txt", "utf8")
 .then(positionsString => {
     const positions = positionsString.split(",").map(position => parseInt(position, 10));
 
     const sorted = positions.sort((a, b) => a > b ? 1 : -1);
 
-    let mid = sorted[Math.floor(sorted.length / 2)];
+    let mid = (sorted[0] + sorted[sorted.length -1]) /2;
 
-    let unique = [ ... new Set(sorted)];
+    let fuel  = getFuel(sorted, mid);
+    let nFuel;
 
-    let rNext = unique[unique.indexOf(mid) + 1];
-    let lNext = unique[unique.indexOf(mid) - 1];
+    let dir = 1;
 
-    let midDist = getFuel(sorted, mid);
-    let lDist   = getFuel(sorted, lNext);
-    let rDist   = getFuel(sorted, rNext);
-
-    if (lDist < midDist) {
-        while (lDist < midDist && iterations < MAX) {
-            iterations++;
-            midDist = lDist;
-
-            mid = lNext;
-            lDist = getFuel(sorted, lNext);
-            lNext = unique[unique.indexOf(mid) - 1];
-        }
+    if (getFuel(sorted, mid + 1) > fuel) {
+        dir = -1;
     }
 
-    if (rDist < midDist) {
-        while (rDist < midDist && iterations < MAX) {
-            iterations++;
-            midDist = rDist;
+    while ((!nFuel || nFuel < fuel) && iterations < MAX_ITERATIONS) {
+        iterations++;
 
-            mid = rNext;
-            rDist = getFuel(sorted, rNext);
-            rNext = unique[unique.indexOf(mid) + 1];
-        }
+        mid += dir;
+        fuel = nFuel || fuel;
+
+        nFuel = getFuel(sorted, mid);
     }
 
+    mid -= dir;
 
-    console.log({ midDist, mid });
+    console.log({ mid, fuel  });
 });
